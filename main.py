a11bf9aedb01decf1f24fdd584712bd7fcd0f50b
icicala <icicala.ubb@gmail.com>
2022-12-04 20:20:01 +0100

    feat(broker) Producer & Consumer Minimum Viable Product

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from kafka import KafkaProducer
+from kafka import KafkaConsumer
+from json import dumps
+from json import loads
 
 
-# Press the green button in the gutter to run the script.
+def producer():
+    kafka_producer = KafkaProducer(
+        bootstrap_servers=['localhost:9092'],
+        value_serializer=lambda x: dumps(x).encode('utf-8')
+    )
+
+    for i in range(10):
+        data = {'counter': i}
+        kafka_producer.send('topic_test', value=data)
+
+
+def consumer():
+    kafka_consumer = KafkaConsumer(
+        'topic_test',
+        bootstrap_servers=['localhost:9092'],
+        auto_offset_reset='earliest',
+        enable_auto_commit=True,
+        group_id='version-1',
+        value_deserializer=lambda x: loads(x.decode('utf-8'))
+    )
+    for event in kafka_consumer:
+        event_data = event.value
+        # manipulation of data
+        print(event_data)
+
+
 if __name__ == '__main__':
-    print_hi('Apache Kafka')
+    # producer()
+    consumer()
